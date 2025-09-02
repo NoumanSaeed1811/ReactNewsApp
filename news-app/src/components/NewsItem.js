9d0c31f666106a9c94648dff687a95af7218d787
NoumanSaeed1811 <saeednouman310@gmail.com>
2025-09-02 15:21:34 +0500

    Update Headings

--- news-app/src/components/NewsItem.js
+++ news-app/src/components/NewsItem.js
@@ -9,11 +9,11 @@ export class NewsItem extends Component {
           <img src={imageUrl} className="card-img-top" alt="/" style={{height: "150px", objectFit: "cover" }}  />
           <div className="card-body">
             <h5 className="card-title">{title}...
-               <span class="badge badge-secondary">{source}</span>
+               <span className="badge badge-secondary">{source}</span>
             </h5>
             <p className="card-text">{desc}...
             </p>
-            <p class="card-text"><small className="text-muted">By {author?author:"unknown"} at {new Date (date).toGMTString()}</small></p>
+            <p className="card-text"><small className="text-muted">By {author?author:"unknown"} at {new Date (date).toGMTString()}</small></p>
             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
               Read More
             </a>
